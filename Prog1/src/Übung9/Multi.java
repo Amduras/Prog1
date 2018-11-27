48e838bbb807245f551eb75410a8bb73b0839185
amduras <Sip90@web.de>
2018-11-27 13:07:11 +0100

    Übung 9 fertig

--- Prog1/src/Übung9/Multi.java
+++ Prog1/src/Übung9/Multi.java
@@ -2,26 +2,45 @@ package Übung9;
 
 public class Multi {
 	
-	private int multi_iter(int a, int b) {
-		int erg=0;
-		
-		return erg;
+	private int mult_iter(int a, int b) {
+		if(b == 0) {
+			return 0;
+		} else if( b == 1) {
+			return a;
+		} else {
+			int erg=0;
+			for(int i = 0; i < Math.abs(b); ++i) {
+				erg = erg+a;
+			}
+			if(b < 0) {
+				erg*=-1;
+			}
+			return erg;
+		}
 	}
 	
-	private int multi_rec(int a, int b) {
-		int erg = 0;
-		
-		return erg;
+	private int mult_rec(int a, int b) {
+		if(b == 0 || a == 0) {
+			return 0;
+		} else if(b == 1) {
+			return a;
+		} else {
+			if(b < 0) {
+				return -a + mult_rec(b+1, a);
+			} else {
+				return a + mult_rec(b-1, a);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
-		int  a = 5;
-		int b = 10;
+		int  a = 3;
+		int b = -2;
 		int erg;
 		Multi mult = new Multi();
-		erg = multi_iter(a,b);
+		erg = mult.mult_iter(a, b);
 		System.out.println("Iterativ: "+erg);
-		erg = multi_rec(a,b);
+		erg = mult.mult_rec(a, b);
 		System.out.println("Rekursiv: "+erg);
 	}
 }
